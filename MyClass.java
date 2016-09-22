677ff469d151da53e9afb26b85a0a7d3d8b20403
treesheal <treesheal@users.noreply.github.com>
2016-09-21 20:10:55 -0500

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class MyClass {
+
+    /**
+     This class converts between two units.
+     */
+
+
+        public static void main(String[] args)
+        {
+
+
+            System.out.println("28 meters = 91.84 feet");
+            System.out.println("168 kilometers = 104.16 miles");
+            System.out.println("47 centimeters = 18.33 inches");
+            System.out.println("27 km/h = 24.6 feet per second");
+            System.out.println("92 liters = 3109.6 fl ounces");
+            System.out.println("200 kilograms = 440 pounds");
+        }
+
+
+}
